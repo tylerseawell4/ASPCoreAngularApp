98c511722c00f8854b0273996e3514dc3ffda79f
tylerseawell4 <tylerseawell4@gmail.com>
2018-08-09 21:20:44 -0400

    initial commit

---
+++ SellingApp.API/SellingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SellingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
